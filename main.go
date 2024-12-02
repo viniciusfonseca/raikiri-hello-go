e04e93724637bcb696270a2fad532c45471882f9
viniciusfonseca <vfonseca1618@gmail.com>
2024-12-02 16:31:55 -0300

    remove unnecessary lines

--- main.go
+++ main.go
@@ -9,9 +9,7 @@ import (
 
 func init() {
 	wasihttp.Handle(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Add("x-wasi-rocks", "true")
 		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("Hello World"))
 		fmt.Fprint(w, "Hello Wasi !!")
 	})
 }
